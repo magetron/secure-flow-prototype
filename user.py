f0d40ee98ea629a0cc270305ff6d4512fb731988
Patrick Wu <magetron1@gmail.com>
2019-06-12 20:14:57 +0100

    minor fix on import

--- user.py
+++ user.py
@@ -1,21 +1,34 @@
+import uuid
+from bid import Bid
+from name import Name
+from card import Card
+from addr import Addr
+
 class User:
     def __init__ (self, name, card, addr):
+        self.__id = uuid.uuid1()
         self.__name = name
         self.__card = card
         self.__addr = addr
 
     @property
+    def id (self):
+        return self.__id
+    
+    @property
     def name (self):
-        return self.__name;
+        return self.__name
 
     @property
     def card (self):
-        return self.__card;
+        return self.__card
 
     @property
     def addr (self):
-        return self.__addr;
+        return self.__addr
 
+    def bid (self, auction, amount):
+        Bid(self, auction, amount)
         
     
     
