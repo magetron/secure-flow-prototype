9d62fd96614ccf0a5579c7204bf23753bb450755
Patrick Wu <magetron1@gmail.com>
2019-06-13 00:51:18 +0100

    add bidding class

---
+++ bid.py
@@ -0,0 +1,18 @@
+import uuid
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+
+class Bid:
+    def __init__(self, bidder, auction, amount):
+        if not auction.started:
+            logging.warning("Auction {} has not started.".format(auction.id))
+        elif auction.bid != None and auction.bid.amount >= amount:
+            logging.error("Bid not successful. Not higher than highest bid.")
+        else:
+            self.__id = uuid.uuid1()
+            self.__bidder = bidder
+            self.__amount = amount
+            self.__auction = auction
+            self.__auction.update_bid(self)
+            logging.info("{} bids {} for auction {} successful.".format(bidder.name.get_full_name, amount, auction.id)) 
