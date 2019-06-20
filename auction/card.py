be31209bb5e10907353b08e8917f63cfb1d839aa
Patrick Wu <magetron1@gmail.com>
2019-06-21 00:38:53 +0100

    add checks for card

--- auction/card.py
+++ auction/card.py
@@ -1,10 +1,25 @@
 class Card:
     def __init__ (self, number, cvc, expiry_year, expiry_month):
-        self.__number = number
-        self.__cvc = cvc
+        self.set_number(number)
+        self.set_cvc(cvc)
+        self.set_expiry_time(expiry_year, expiry_month)
+
+    def set_number (self, number):
+        if len(number) != 16 or not number.isnumeric():
+            raise Exception("Card number should be 16 digits long")
+        else:
+            self.__number = number
+
+    def set_cvc (self, cvc):
+        if len(cvc) != 3 or not cvc.isnumberic():
+            raise Exception("Card CVC should 3 digits long")
+        else:
+            self.__cvc = cvc
+
+    def set_expiry_time (self, expiry_year, expiry_month):
         self.__expiry_year = expiry_year
         self.__expiry_month = expiry_month
-    
+
     @property
     def number (self):
         return self.__number
